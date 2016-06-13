05eb352df2594669aee7098fa7a49c55582afd6c
Sergio Martinez <rc.martinez.sergio@leadps.org>
2016-06-12 19:10:58 -0700

    Problem Setty 9

--- 6-3CaesarsCipher/applyCipher.py
+++ 6-3CaesarsCipher/applyCipher.py
@@ -1,3 +1,5 @@
+# import 
+import string 
 # applyCipher.py
 # A pogram to encrypt/decrypt user text
 # using Caeaser's Cipher
@@ -8,36 +10,49 @@
 # arguments: key
 # returns: dictionary of mapped letters
 def createDictionary(key):
-
-	# placeholder
-	return {}
+	# call alphabet, lowercase and uppercase
+	alpha = string.ascii_lowercase
+	alphaUp = string.ascii_uppercase
+	dict = {}
+	c = 0
+	for l in range(0, len(alpha)):
+		dict[alpha[(l + key) % 26]] = alpha[l]
+	for l in range(0, len(alpha)):
+		dict[alphaUp[(l + key) % 26]] = alphaUp[l]
+	dict[" "] = " "
+	return dict
 # gets the encrypted message from user
 # argument: none
 # return: encoded message
 def getMessage():
-	pass
-
+	print("What message would you like to decode?")
+	message = raw_input()
+	return message
+	
 # for each letter in the message, decodes and records
 # argument: encoded message, dictionary
 # returns: decoded message
 def decodeMessage(message, dictionary):
-	pass
-
+	newMessage = ""
+	for m in message:
+		newMessage = newMessage + dictionary[m]
+	return newMessage
 # outputs the decoded message to the terminal 
 # arguments: decoded message
 # returns: none
 def printMessage(message):
-	pass
+	print(message)
 
 # execution starts here
 
 # ask user for the key
 print("What key would you like to use to decode?")
-
 key = int(raw_input())
+
 
 dictionary = createDictionary(key)
 encodedMessage = getMessage()
 decodedMessage = decodeMessage(encodedMessage, dictionary)
-
+print("Here's the decoding of your message:")
 printMessage(decodedMessage)
+
